d1d161b6ca7f91c839410c066e0d8ea736077396
phucbm <hiphucbui@gmail.com>
2022-09-26 14:14:07 +0700

    :wrench: helpers: add setZIndex

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,13 @@
+import {loopFromIndex, setCSS} from "@/utils";
+
 export function getSlideByIndex(context, index){
     return context.slides[index];
+}
+
+export function setZIndex(context){
+    let countTemp = 0;
+    loopFromIndex(context.slideCount, context.currentIndex, (index, count) => {
+        setCSS(getSlideByIndex(context, index), {zIndex: `${count - countTemp}`});
+        countTemp++;
+    });
 }
